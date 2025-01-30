ca1c8e4dfeb21b756e20aed9d3047d0b2d8cda13
oOXxTNTxXOo <bob.bross10@gmail.com>
2025-01-29 22:52:12 -0700

    ok

---
+++ scan.py
@@ -0,0 +1,15 @@
+#scans ip address for address and pings for information on up or down
+
+# pip install scapy dnspython
+
+from collections import Counter
+from scapy.all import ARP, sniff
+
+def arp_display(pkt):
+    if pkt[ARP].op == 1: #who-has (request)
+        return f"Request: {pkt[ARP].psrc} is asking about {pkt[ARP].pdst}"
+    if pkt[ARP].op == 2: #is-at (response)
+        return f"*Response: {pkt[ARP].hwsrc} has address {pkt[ARP].psrc}"
+
+sniff(prn=arp_display, filter="arp", store=0, count=10)
+                            
